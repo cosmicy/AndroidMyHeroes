54f002d8b7861f2ca6ef3c4e7c62a07ffb00a3c7
cosmi <cosmicy@outlook.com>
2016-02-24 21:07:54 +0800

    +新建项目C51

---
+++ C51_ViewScroll/app/src/androidTest/java/com/cosmicy/cosmi/c51_viewscroll/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.cosmicy.cosmi.c51_viewscroll;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
